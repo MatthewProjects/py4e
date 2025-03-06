1ca750426fedbf039b1e76358081c63cdb2c6488
Matt Troughton <mvt0408@gmail.com>
2025-03-05 18:25:01 -0700

    Update ex_02_02.py

--- ex_02_02/ex_02_02.py
+++ ex_02_02/ex_02_02.py
@@ -1,5 +1,5 @@
 #giving an input to the user and getting the output
-# with this snippet from ex_02_02/ex_02_02.py:
+#with this snippet from ex_02_02/ex_02_02.py:
 name = input('Enter your name: ')
 print('Hello', name)
 # Output: Hello Sarah
